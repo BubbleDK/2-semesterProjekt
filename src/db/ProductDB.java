cc2a433f9919308f17dada840d8c0148fb48706d
Rasmus <neskidug@gmail.com>
2022-04-21 15:37:22 +0200

    findByProductBarcode metode lavet

--- src/db/ProductDB.java
+++ src/db/ProductDB.java
@@ -2,17 +2,41 @@ package db;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import ctrl.DataAccessException;
 import model.Product;
 
 public class ProductDB implements ProductDBIF {
 	private Product currProduct;
 	private static final String FIND_BY_BARCODE_Q = "SELECT * FROM KK_Product WHERE barcode = ?";
 	private static PreparedStatement findByBarcodePS;
+	
+	public ProductDB() {
+		try {
+			findByBarcodePS = DBConnection.getInstance().getConnection().prepareStatement(FIND_BY_BARCODE_Q);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Override
-	public Product findByProductBarcode(String barcode) {
-		// TODO Auto-generated method stub
+	public Product findByProductBarcode(String barcode) throws DataAccessException {
+		currProduct = null;
+		try {
+			findByBarcodePS.setString(1, barcode);
+			ResultSet rs = findByBarcodePS.executeQuery();
+			if(rs.next()) {
+				currProduct = buildPackObject(rs,barcode);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(DBMessages.COULD_NOT_BIND_OR_EXECUTE_QUERY, e);
+//			e.printStackTrace();
+		}
 		return null;
 	}
 	
--- src/db/ProductDBIF.java
+++ src/db/ProductDBIF.java
@@ -1,7 +1,8 @@
 package db;
 
+import ctrl.DataAccessException;
 import model.Product;
 
 public interface ProductDBIF {
-	Product findByProductBarcode(String barcode);
+	Product findByProductBarcode(String barcode) throws DataAccessException;
 }
