3ad44c24de090f8e20aea28bccd83388bf0689ef
MADUCN <93515405+MADUCN@users.noreply.github.com>
2022-04-26 10:35:03 +0200

    RandomAlphaNumericString

--- src/model/B2BLogin.java
+++ src/model/B2BLogin.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.nio.charset.Charset;
+import java.util.Random;
+
 public class B2BLogin {
 	
 	private String email;
@@ -9,9 +12,43 @@ public class B2BLogin {
 		this.email = email;
 	}
 	
-	private void createRandomGiftNo() {
+	
+	
+	private String createRandomGiftNo(String email) {
+		getRandomString();
 		
+		
+		
+		return "Hej";
 	}
+
+	private String getRandomString() {
+		byte[] array = new byte[256];
+		new Random().nextBytes(array);
+		int maxLength = 20;
+		
+		String randomString = new String(array, Charset.forName("UTF-8"));
+		
+		StringBuffer buffer = new StringBuffer();
+		
+		String AlphaNumericString = randomString.replaceAll("[^A-Za-z0-9]", "");
+		
+		for(int i = 0; i < AlphaNumericString.length(); i++) {
+			
+			if(Character.isLetter(AlphaNumericString.charAt(i)) && (maxLength > 0)
+				|| Character.isDigit(AlphaNumericString.charAt(i)) && (maxLength > 0)){
+				
+				buffer.append(AlphaNumericString.charAt(i));
+				maxLength--;
+			}
+		}
+		
+		this.giftNo = buffer.toString();
+		
+		return giftNo;		
+	}
+
+
 
 	public String getEmail() {
 		return email;
