a2b065e78d7b25d190b525728e629534fb8e65ff
Rasmus <neskidug@gmail.com>
2022-05-04 21:05:55 +0200

    sæt companyname på ordre

--- src/gui/B2BOrderMenu.java
+++ src/gui/B2BOrderMenu.java
@@ -28,6 +28,7 @@ import model.B2BCustomer;
 public class B2BOrderMenu extends JFrame {
 
 	private JPanel contentPane;
+	private CustomerCtrl customerCtrl;
 
 	/**
 	 * Launch the application.
@@ -55,16 +56,16 @@ public class B2BOrderMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{60, 60, 60, 60, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 60, 60, 60, 60, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JButton btnNyB2BOrdre = new JButton("Ny B2B Ordre");
 		btnNyB2BOrdre.addActionListener((e -> {
 			newB2BOrderClicked();
@@ -75,7 +76,7 @@ public class B2BOrderMenu extends JFrame {
 		gbc_btnNyB2BOrdre.gridx = 0;
 		gbc_btnNyB2BOrdre.gridy = 0;
 		panel.add(btnNyB2BOrdre, gbc_btnNyB2BOrdre);
-		
+
 		JButton btnNewButton = new JButton("B2B Gavevalg");
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.fill = GridBagConstraints.BOTH;
@@ -83,7 +84,7 @@ public class B2BOrderMenu extends JFrame {
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 1;
 		panel.add(btnNewButton, gbc_btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("New button");
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.fill = GridBagConstraints.BOTH;
@@ -91,42 +92,79 @@ public class B2BOrderMenu extends JFrame {
 		gbc_btnNewButton_1.gridx = 0;
 		gbc_btnNewButton_1.gridy = 2;
 		panel.add(btnNewButton_1, gbc_btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton("New button");
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
 		gbc_btnNewButton_2.fill = GridBagConstraints.BOTH;
 		gbc_btnNewButton_2.gridx = 0;
 		gbc_btnNewButton_2.gridy = 3;
 		panel.add(btnNewButton_2, gbc_btnNewButton_2);
+
+		init();
+	}
+
+	private void init() {
+		try {
+			customerCtrl = new CustomerCtrl();
+		} catch (DataAccessException e) {
+			JOptionPane.showMessageDialog(this, "Kan ikke få adgang til database", "Data access error",
+					JOptionPane.OK_OPTION);
+//			e.printStackTrace();
+		}
 	}
 
 	private void newB2BOrderClicked() {
-		String companyName = createPopup();
-		String endDate = JOptionPane.showInputDialog("Indtast slut dato: 'DD-MM-YYYY'");
-		B2BOrderGUI orderGUI = new B2BOrderGUI(companyName, endDate);
-		orderGUI.setVisible(true);
-		this.dispose();
-		
+		String companyName = findCompanyNameByCVR();
+		System.out.println("efter findcompanybycvr");
+		if(companyName != null) {
+			String endDate = JOptionPane.showInputDialog("Indtast slut dato: 'DD-MM-YYYY'");
+			B2BOrderGUI orderGUI = new B2BOrderGUI(companyName, endDate);
+			orderGUI.setVisible(true);
+			this.dispose();
+		}else {
+			newB2BOrderClicked();
+		}
 	}
-	//TODO den kører metoden to gange hvis man taster forkert første gang
-	private String createPopup() {
+
+	private String findCompanyNameByCVR() {
+		System.out.println("1");
 		String insertCVR = JOptionPane.showInputDialog("Indtast CVR");
-		int cvr = Integer.parseInt(insertCVR);
-		CustomerCtrl customerCtrl;
+		int cvr = -1;
+		if (isANumber(insertCVR) && !insertCVR.isBlank()) {
+			cvr = Integer.parseInt(insertCVR);
+			System.out.println("2");
+		} else {
+			JOptionPane.showMessageDialog(this, "Antal skal være et tal.", "Input fejl", JOptionPane.OK_OPTION);
+			System.out.println("3");
+		}
+
 		B2BCustomer currCustomer = null;
-		
+
 		try {
-			customerCtrl = new CustomerCtrl();
 			currCustomer = customerCtrl.findB2BCustomer(cvr);
+			System.out.println(currCustomer + "har fundet en customer");
 		} catch (DataAccessException e1) {
 			e1.printStackTrace();
-			JOptionPane.showMessageDialog(this, "Kan ikke få adgang til database", "Data access error", JOptionPane.OK_OPTION);
+			JOptionPane.showMessageDialog(this, "Kan ikke få adgang til database", "Data access error",
+					JOptionPane.OK_OPTION);
 		}
-		
-		if(currCustomer == null) {
-				JOptionPane.showMessageDialog(null, "Kunne ikke finde kunde ud fra indtastede cvr.", "Fejlmeddelelse", JOptionPane.OK_OPTION);
-				newB2BOrderClicked();
-			}
+
+		if (currCustomer == null) {
+			JOptionPane.showMessageDialog(null, "Kunne ikke finde kunde ud fra indtastede cvr.", "Fejlmeddelelse",
+					JOptionPane.OK_OPTION);
+			System.out.println("4");
+			return null;
+		}
+		System.out.println(currCustomer + "2");
 		return currCustomer.getCompanyName();
+	}
+
+	private boolean isANumber(String string) {
+		try {
+			Double.parseDouble(string);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
