37142d7a2f84e12460fdb4775b2d73af844be487
MADUCN <93515405+MADUCN@users.noreply.github.com>
2022-05-12 12:34:19 +0200

    TEst dbconnection

--- src/tests/TestDatabaseConnection.java
+++ src/tests/TestDatabaseConnection.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,18 +13,25 @@ import db.DBConnection;
 import exceptions.DataAccessException;
 
 class TestDatabaseConnection {
-
+	private static Connection dbCon;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+	
 	}
 
 	@Test
 	void testDatabaseConnection() throws DataAccessException, SQLException {
-		Connection dbCon = DBConnection.getInstance().getConnection();
+		dbCon = DBConnection.getInstance().getConnection();
 		boolean reachable = dbCon.isValid(10);
 		assertEquals(true, reachable);
-		dbCon.close();
 		
 	}
+	
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		dbCon.close();
+	}
+
+	
 
 }
