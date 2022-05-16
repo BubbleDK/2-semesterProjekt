38655213e356c23b0a33bd924273093cf54de868
Rasmus <neskidug@gmail.com>
2022-05-16 13:30:05 +0200

    FindByProductID lavet i productDB

--- src/db/ProductDB.java
+++ src/db/ProductDB.java
@@ -16,12 +16,15 @@ public class ProductDB implements ProductDBIF {
 	private static PreparedStatement findByBarcodePS;
 	private static final String FIND_PRICEHISTORY_BY_PRODUCTID_Q = "SELECT TOP 1 * FROM kk_Pricehistory WHERE productID = ? ORDER BY historyDate DESC";
 	private static PreparedStatement findPriceHistoryPS;
+	private static final String FIND_BY_PRODUCTID_Q = "SELECT * FROM kk_AbstractProduct WHERE id = ?";
+	private static PreparedStatement findByProductIDPS;
 	
 	public ProductDB() throws DataAccessException {
 		Connection con = DBConnection.getInstance().getConnection();
 		try {
 			findByBarcodePS = con.prepareStatement(FIND_BY_BARCODE_Q);
 			findPriceHistoryPS = con.prepareStatement(FIND_PRICEHISTORY_BY_PRODUCTID_Q);
+			findByProductIDPS = con.prepareStatement(FIND_BY_PRODUCTID_Q);
 		} catch (SQLException e) {
 //			e.printStackTrace();
 			throw new DataAccessException(DBMessages.COULD_NOT_PREPARE_STATEMENT, e);
@@ -61,6 +64,22 @@ public class ProductDB implements ProductDBIF {
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DataAccessException(DBMessages.COULD_NOT_READ_RESULTSET, e);
+		}
+		return currPack;
+	}
+
+	@Override
+	public Pack findByProductId(int id) throws DataAccessException {
+		Pack currPack = null;
+		try {
+			findByBarcodePS.setInt(1, id);
+			ResultSet rs = findByProductIDPS.executeQuery();
+			if(rs.next()) {
+				currPack = buildPackObject(rs);
+			}
+		} catch (SQLException e) {
+//			e.printStackTrace();
+			throw new DataAccessException(DBMessages.COULD_NOT_BIND_OR_EXECUTE_QUERY, e);
 		}
 		return currPack;
 	}
--- src/db/ProductDBIF.java
+++ src/db/ProductDBIF.java
@@ -7,4 +7,6 @@ import model.Product;
 
 public interface ProductDBIF {
 	Pack findByProductBarcode(String barcode) throws DataAccessException;
+	
+	Pack findByProductId(int id) throws DataAccessException;
 }
